d3cdf88c57073dc5606c8b9b18b95aea56170f1a
jhalake <jatenihaleke@yahoo.com>
2016-11-23 03:33:22 +0300

    site file modified

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,7 +47,7 @@ class { 'sudo':
  user {'quri':
       ensure => 'present',
       uid    => '5006',
-      gid    => 'admins',
+      gid    => 'admin',
       home   => '/home/quri',
       shell  => '/bin/bash',
       managehome => true,
